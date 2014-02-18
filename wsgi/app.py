500890e34da16650641fad9f9df03bcfec5adc40
a3gis <henri.maurer@gmail.com>
2014-02-18 11:22:00 +0000

    install flask-oauth

--- wsgi/app.py
+++ wsgi/app.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, url_for, request
+from flask_oauth import OAuth
+
 app = Flask(__name__)
+oauth = OAuth()
+
+facebook = oauth.remote_app('facebook',
+    base_url='https://graph.facebook.com/',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    authorize_url='https://www.facebook.com/dialog/oauth',
+    consumer_key="1466224973600976",
+    consumer_secret="26960e4d7278e704e103525b338fd8f1",
+    request_token_params={'scope': 'email'}
+)
 
 @app.route("/")
 def hello():
     return "Hello ILWhack 2014!."
 
+@app.route('/login')
+def login():
+    return facebook.authorize(callback="http://localhost/")
+
 if __name__ == "__main__":
-    app.run()
+    app.debug = True
+    app.config['SECRET_KEY'] = "muchsecret"
+    app.run(port = 80)
 
