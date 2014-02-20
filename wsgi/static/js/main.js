6f5a66600b80112f7694b5bcb1f2b7f6e06c3fed
Hamish Hutchings <hamhut1066@gmail.com>
2014-02-20 18:02:19 +0000

    add selecting

--- wsgi/static/js/main.js
+++ wsgi/static/js/main.js
@@ -33,6 +33,9 @@ $(document).ready( function () {
         }
     });
 
+    $(".friendlist tr").on("click", function() {
+        $(this).toggleClass("active");
+    }
     $('#search').on("keyup", function () {
         var q = $('#search').val();
         $(".friend-name").each(function () {
