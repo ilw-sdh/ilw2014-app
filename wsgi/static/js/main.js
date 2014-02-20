122936b61d0be34c4aad81d94670c6dbf7b5504d
AngusP <a7cp3ar50n@gmail.com>
2014-02-20 18:43:48 +0000

    Modified modal

--- wsgi/static/js/main.js
+++ wsgi/static/js/main.js
@@ -95,9 +95,9 @@ function prepare_flight(dest) {
     var rel_date = moment(dest.InboundLeg.DepartureDate).fromNow();
     //add a 'time until' event using .fromNow() as a tooltip
     var r = "<tr>";
-    r += "<td class='date'><div class='date-tooltip' data-toggle='tooltip' data-placement='auto left' title='"+rel_date+"'></div>"+date+"</td>";
-    r += "<td class='carrier'>"+dest.InboundLeg.Carrier+"</td>";
-    r += "<td class='price'><button class='btn btn-success'>&pound;"+dest.MinPrice+"</button></td>";
+    r += "<td class=\"date\"><div class=\"date-tooltip\" data-toggle=\"tooltip\" data-placement=\"auto left\" title=\""+rel_date+"\"></div>"+date+"</td>";
+    r += "<td class=\"carrier\">"+dest.InboundLeg.Carrier+"</td>";
+    r += "<td class=\"price\"><span class=\"label label-default\">&pound;"+dest.MinPrice+"</td>";
     r += "</tr>";
     return r;
 }
