c61795984a90a272bfb5e8611c158283e826300f
Hamish Hutchings <hamhut1066@gmail.com>
2014-02-20 21:38:00 +0000

    random text

--- wsgi/static/js/main.js
+++ wsgi/static/js/main.js
@@ -43,6 +43,7 @@ $(document).ready( function () {
             } else {
                 $(this).parent().parent().hide();
             }
+            console.log(any_selected);
         });
         $(this).toggleClass("selected");
     });
